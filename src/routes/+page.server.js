978c795013341ad1313867be0de2c537a4a7bfbf
Andrew <74398819+AndrewDTR@users.noreply.github.com>
2026-01-14 02:20:58 -0500

    day 15

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -5,21 +5,21 @@ export async function load({ url }) {
         redirect(308, 'https://goatpenguin.com');
     }
 
-    const article = "Vocaloid";
-    const friendly = "Vocaloid";
-    const acceptedGuesses = ["vocaloid"];
-    const day = "day14";
-    const dayNum = 14;
+    const article = "Dead_Internet_theory";
+    const friendly = "Dead Internet theory";
+    const acceptedGuesses = ["dead internet theory", "dead internet"];
+    const day = "day15";
+    const dayNum = 15;
 
-    const categories = ["2000 establishments in Spain",
-        "2004 software",
-        "Japanese inventions",
-        "Music production software",
-        "Singing software synthesizers",
+    const categories = ["Internet-related controversies",
+        "2020s neologisms",
+        "Mass media issues",
+        "Science and technology-related conspiracy theories",
+        "Internet bots",
     ]
 
-    const blurb = `Vocaloid (ボーカロイド, Bōkaroido) is a singing voice synthesizer software product. Its signal processing part was developed through a joint research project between Yamaha Corporation and the Music Technology Group at Pompeu Fabra University, Barcelona. The software was ultimately developed into the commercial product "Vocaloid" that was released in 2004.`;
-    const img = "https://upload.wikimedia.org/wikipedia/en/1/14/VOCALOID_6.png";
+    const blurb = `The dead Internet theory (also referred to as the dead Internet or the dead Internet conspiracy theory) is a conspiracy theory that asserts that since around 2016, the Internet has consisted mainly of bot activity and automatically generated content manipulated by algorithmic curation, as part of a coordinated and intentional effort to control the population and minimize organic human activity. Proponents of the theory believe these social bots were created intentionally to help control algorithms and boost search results in order to influence consumers. Some proponents of the theory accuse government agencies of using bots to manipulate public perception, opinions and views. The dead Internet theory has gained traction because many of the phenomena mentioned in connection with it are quantifiable, such as increased bot traffic, but the evidence does not support the theory itself.`;
+    const img = "https://upload.wikimedia.org/wikipedia/commons/5/5f/404_not_found.png";
 
     return {
         article,
