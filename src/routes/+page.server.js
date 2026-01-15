88ee99929e20b9e8f9aca690f447dca5dca4a8e2
Andrew <74398819+AndrewDTR@users.noreply.github.com>
2026-01-15 01:10:16 -0500

    day 16

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -5,21 +5,21 @@ export async function load({ url }) {
         redirect(308, 'https://goatpenguin.com');
     }
 
-    const article = "Dead_Internet_theory";
-    const friendly = "Dead Internet theory";
-    const acceptedGuesses = ["dead internet theory", "dead internet"];
-    const day = "day15";
-    const dayNum = 15;
+    const article = "Tetris";
+    const friendly = "Tetris";
+    const acceptedGuesses = ["tetris"];
+    const day = "day16";
+    const dayNum = 16;
 
-    const categories = ["Internet-related controversies",
-        "2020s neologisms",
-        "Mass media issues",
-        "Science and technology-related conspiracy theories",
-        "Internet bots",
+    const categories = ["1990s fads and trends",
+        "NP-complete problems",
+        "Soviet inventions",
+        "Video game franchises",
+        "Falling block puzzle games",
     ]
 
-    const blurb = `The dead Internet theory (also referred to as the dead Internet or the dead Internet conspiracy theory) is a conspiracy theory that asserts that since around 2016, the Internet has consisted mainly of bot activity and automatically generated content manipulated by algorithmic curation, as part of a coordinated and intentional effort to control the population and minimize organic human activity. Proponents of the theory believe these social bots were created intentionally to help control algorithms and boost search results in order to influence consumers. Some proponents of the theory accuse government agencies of using bots to manipulate public perception, opinions and views. The dead Internet theory has gained traction because many of the phenomena mentioned in connection with it are quantifiable, such as increased bot traffic, but the evidence does not support the theory itself.`;
-    const img = "https://upload.wikimedia.org/wikipedia/commons/5/5f/404_not_found.png";
+    const blurb = `Tetris (Russian: Тетрис) is a puzzle video game created by Alexey Pajitnov, a Soviet software engineer. In typical Tetris gameplay, falling tetromino shapes must be neatly sorted into a pile. Once a horizontal line of the game board is filled in, it disappears, granting points and preventing the pile from overflowing. Since its initial creation, this gameplay has been used in over 220 versions, released for over 70 platforms. Newer versions frequently implement additional game mechanics, some of which have become standard over time. As of December 2024, these versions collectively serve as the second-best-selling video game series with over 520 million sales, mostly on mobile devices.`;
+    const img = "https://upload.wikimedia.org/wikipedia/commons/4/46/Tetris_logo.png";
 
     return {
         article,
