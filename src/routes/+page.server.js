fe98702bcc4fe17398c2cee1fd8f5b46d8802166
Andrew <74398819+AndrewDTR@users.noreply.github.com>
2026-01-19 03:00:13 -0600

    day 20

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -5,21 +5,21 @@ export async function load({ url }) {
         redirect(308, 'https://goatpenguin.com');
     }
 
-    const article = "Boston_Tea_Party";
-    const friendly = "Boston Tea Party";
-    const acceptedGuesses = ["the boston tea party", "boston tea party", "tea party"];
-    const day = "day19";
-    const dayNum = 19;
+    const article = "Rubik%27s_Cube";
+    const friendly = "Rubik's Cube";
+    const acceptedGuesses = ["rubiks cube", "rubik's cube", "rubix cube"];
+    const day = "day20";
+    const dayNum = 20;
 
-    const categories = ["1770s riots",
-        "Battles and conflicts without fatalities",
-        "1773 in the Province of Massachusetts Bay",
-        "Tax resistance in the United States",
-        "History of tea",
+    const categories = ["Single-player games",
+        "Hungarian inventions",
+        "1980s fads and trends",
+        "Educational toys",
+        "Logic puzzles",
     ]
 
-    const blurb = `The Boston Tea Party was an act of protest on December 16, 1773 during the American Revolution. Initiated by the Sons of Liberty in Boston, the capital of Massachusetts, one of the Thirteen Colonies of British America, it escalated hostilities between Great Britain and the Patriots, who opposed British policy towards its American colonies.     Less than two years later, on April 19, 1775, the Battles of Lexington and Concord, also in Massachusetts, launched the eight-year American Revolutionary War, which resulted in the independence of the colonies as the United States.`;
-    const img = "https://upload.wikimedia.org/wikipedia/commons/e/e6/Boston_Tea_Party_w.jpg";
+    const blurb = `The Rubik's Cube is a 3D combination puzzle invented in 1974 by Hungarian sculptor and professor of architecture Ernő Rubik. Originally called the Magic Cube, the puzzle was licensed by Rubik to be sold by Pentangle Puzzles in the UK in 1978, and then by Ideal Toy Corp in 1980 via businessman Tibor Laczi and Seven Towns founder Tom Kremer. The cube was released internationally in 1980 and became one of the most recognised icons in popular culture. It won the 1980 German Game of the Year special award for Best Puzzle. As of January 2024, around 500 million cubes had been sold worldwide, making it the world's bestselling puzzle game and bestselling toy. The Rubik's Cube was inducted into the US National Toy Hall of Fame in 2014.`;
+    const img = "https://upload.wikimedia.org/wikipedia/commons/6/61/Rubiks_cube_solved.jpg";
 
     return {
         article,
