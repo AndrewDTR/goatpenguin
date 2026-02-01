c7cfaf5abbab1140c8108b562975a09aae3636eb
Andrew <74398819+AndrewDTR@users.noreply.github.com>
2026-02-01 00:53:51 -0600

    day 33

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -5,21 +5,21 @@ export async function load({ url }) {
         redirect(308, 'https://goatpenguin.com');
     }
 
-    const article = "Silly_Bandz";
-    const friendly = "Silly Bandz";
-    const acceptedGuesses = ["silly bandz", "silly bands"];
-    const day = "day32";
-    const dayNum = 32;
+    const article = "Steinway_%26_Sons";
+    const friendly = "Steinway & Sons";
+    const acceptedGuesses = ["steinway", "steinway and sons", "steinway & sons"];
+    const day = "day33";
+    const dayNum = 33;
 
-    const categories = ["Products introduced in 2002",
-        "Toy collecting",
-        "2010s fashion",
-        "Silicone toys",
-        "Bracelets",
+    const categories = ["Companies based in Queens, New York",
+        "German brands",
+        "Purveyors to the Russian imperial family",
+        "Luxury brands",
+        "Piano manufacturing companies",
     ]
 
-    const blurb = `Silly Bandz are rubber bands made of silicone rubber formed into shapes including animals, objects, numbers, and letters. They are normally worn as bracelets. Silly Bandz retail in packages with select themes, such as princesses or animals, and fashion accessories. Similarly shaped silicone bands are also available under other brand names from a variety of companies. Silly Bandz were especially popular in the early 2010s.`;
-    const img = "https://upload.wikimedia.org/wikipedia/commons/9/9a/Shaped_Rubber_Bands.JPG";
+    const blurb = `Steinway & Sons, also known as Steinway, is a German-American piano company, founded in 1853 in New York City by German piano builder Heinrich Engelhard Steinweg (later known as Henry E. Steinway). The company's growth led to a move to a larger factory in New York, and later opening an additional factory in Hamburg, Germany. The New York factory, in the borough of Queens, supplies the Americas, and the factory in Hamburg supplies the rest of the world.`;
+    const img = "https://upload.wikimedia.org/wikipedia/commons/8/8d/Steinway_%26_Sons_concert_grand_piano%2C_model_D-274%2C_manufactured_at_Steinway%27s_factory_in_Hamburg%2C_Germany.png";
 
     return {
         article,
