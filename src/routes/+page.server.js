b595a1c7f31f377f670fd8c607a7e40a28857c20
Andrew <74398819+AndrewDTR@users.noreply.github.com>
2026-02-03 00:47:29 -0600

    day 35

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -5,21 +5,21 @@ export async function load({ url }) {
         redirect(308, 'https://goatpenguin.com');
     }
 
-    const article = "Doomsday_Clock";
-    const friendly = "Doomsday Clock";
-    const acceptedGuesses = ["doomsday clock"];
-    const day = "day34";
-    const dayNum = 34;
+    const article = "Deflategate";
+    const friendly = "Deflategate";
+    const acceptedGuesses = ["deflategate", "deflate gate"];
+    const day = "day35";
+    const dayNum = 35;
 
-    const categories = ["Doomsday scenarios",
-        "Political symbols",
-        "Symbols introduced in 1947",
-        "Alert measurement systems",
-        "Clocks",
+    const categories = ["2015 in sports in Massachusetts",
+        "New England Patriots postseason",
+        "NFL controversies",
+        "Banned sports tactics",
+        "Cheating in sports",
     ]
 
-    const blurb = [`The Doomsday Clock is a symbol that represents the estimated likelihood of a human-made global catastrophe, in the opinion of the nonprofit organization Bulletin of the Atomic Scientists.`, `The Clock's original setting in 1947 was seven minutes to midnight. It has since been set backward eight times and forward 19 times. The farthest time from midnight was 17 minutes in 1991, and the closest is 85 seconds in 2026.`, `The Clock was moved to 150 seconds (2 minutes, 30 seconds) in 2017, then forward to two minutes to midnight in 2018, and left unchanged in 2019. It was moved forward to 100 seconds (1 minute, 40 seconds) in 2020, 90 seconds (1 minute, 30 seconds) in 2023, 89 seconds (1 minute, 29 seconds) in 2025, and 85 seconds (1 minute, 25 seconds) in 2026.`]
-    const img = "/clock.jpg";
+    const blurb = `The Deflategate scandal was a National Football League (NFL) controversy in the United States involving the allegation that New England Patriots quarterback Tom Brady ordered the deliberate deflation of footballs that were used in the Patriots' victory against the Indianapolis Colts during the 2014 AFC Championship Game on January 18, 2015. The controversy resulted in Brady being suspended for the first four games of the 2016 NFL season, while the Patriots were fined $1 million and forfeited two draft selections in 2016.`;
+    const img = "https://upload.wikimedia.org/wikipedia/commons/7/73/25th_Laureus_World_Sports_Awards_-_Red_Carpet_-_Tom_Brady_-_240422_191334_%28cropped%29_%28cropped%29.jpg";
 
     return {
         article,
