1851e18d78bd85c78628934244a4c54d8ff7b5d2
Andrew <74398819+AndrewDTR@users.noreply.github.com>
2026-02-06 01:16:59 -0600

    day 38

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -5,21 +5,21 @@ export async function load({ url }) {
         redirect(308, 'https://goatpenguin.com');
     }
 
-    const article = "Air_Force_One";
-    const friendly = "Air Force One";
-    const acceptedGuesses = ["air force one", "air force 1"];
-    const day = "day37";
-    const dayNum = 37;
+    const article = "Leet";
+    const friendly = "Leet";
+    const acceptedGuesses = ["leet", "1337", "l33t", "leetspeak", "leet speak", "l33t speak"];
+    const day = "day38";
+    const dayNum = 38;
 
-    const categories = ["Boeing 747",
-        "1953 establishments in the United States",
-        "United States Air Force",
-        "United States special-purpose aircraft",
-        "Presidential aircraft",
+    const categories = ["Internet memes",
+        "1990s slang",
+        "Latin-script representations",
+        "Encodings",
+        "Nerd culture",
     ]
 
-    const blurb = `Air Force One is the official air traffic control-designated call sign for a United States Air Force aircraft carrying the president of the United States. The term is commonly used to denote U.S. Air Force aircraft modified and used to transport the president, and as a metonym for the primary presidential aircraft, VC-25, although it can be used to refer to any Air Force aircraft the president travels on.`;
-    const img = "https://upload.wikimedia.org/wikipedia/commons/4/45/United_States_Air_Force_Boeing_VC-25_%2892-9000%29_landing_at_Dayton_International_Airport_%281%29.jpg";
+    const blurb = `Leet (or "1337"), also known as eleet, leetspeak, or simply hacker speech, is a system of modified spellings used primarily on the Internet. It often uses character replacements in ways that play on the similarity of their glyphs via reflection or other resemblance. Additionally, it modifies certain words on the basis of a system of suffixes and alternative meanings. There are many dialects or linguistic varieties in different online communities.`;
+    const img = "https://upload.wikimedia.org/wikipedia/commons/4/40/Laugh-Out-Loud_Cats_736.jpg";
 
     return {
         article,
