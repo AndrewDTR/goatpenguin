be83e1107a83c1bd58085fac358217c42a4cb884
Andrew <74398819+AndrewDTR@users.noreply.github.com>
2026-02-28 00:45:47 -0600

    day 48

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -5,21 +5,21 @@ export async function load({ url }) {
         redirect(308, 'https://goatpenguin.com');
     }
 
-    const article = "E3";
-    const friendly = "E3";
-    const acceptedGuesses = ["e3", "e 3", "e three", "ethree", "electronic entertainment expo"];
-    const day = "day47";
-    const dayNum = 47;
+    const article = "Think_of_the_children";
+    const friendly = "Think of the children";
+    const acceptedGuesses = ["wont someone think of the children", "wont someone please think of the children", "think of the children", "please think of the children", "wont anyone please think of the children", "wont anyone think of the children", "what about the children", "please what about the children"];
+    const day = "day48";
+    const dayNum = 48;
 
-    const categories = ["Annual events in Los Angeles County, California",
-        "2023 disestablishments in California",
-        "Trade shows in the United States",
-        "Organizations disestablished due to the COVID-19 pandemic",
-        "Video gaming in the United States",
+    const categories = ["Political neologisms",
+        "Appeals to emotion",
+        "Political terminology of the United States",
+        "Childhood",
+        "Rhetoric",
     ]
 
-    const blurb = `E3 (short for Electronic Entertainment Expo) was an annual trade event for the video game industry organized and presented by the Entertainment Software Association (ESA). It was held principally in Los Angeles from 1995 to 2019, with its final iteration held virtually in 2021. The event hosted developers, publishers, hardware manufacturers, and other industry professionals who used the occasion to introduce and advertise upcoming games, hardware, and merchandise to the press. During its existence, E3 was the world's largest and most prestigious annual gaming expo.`;
-    const img = "a.png";
+    const blurb = `"Think of the children" (also "What about the children?") is a cliché that evolved into a rhetorical tactic. In the literal sense, it refers to children's rights (as in discussions of child labor). In debate, it is a plea for pity that is used as an appeal to emotion, and therefore may become a logical fallacy.`;
+    const img = "https://upload.wikimedia.org/wikipedia/en/transcoded/e/e1/Think_of_the_children.webm/Think_of_the_children.webm.360p.webm";
 
     return {
         article,
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -271,10 +271,10 @@
 			</div>
 			<div class="mt-1">
 				{#if img !== null}
-					<img
-						class="my-2 h-48 w-auto border border-white p-2 sm:float-none md:float-right md:m-4"
+					<video
+						class="my-2 h-64 w-auto border border-white p-2 sm:float-none md:float-right md:m-4"
 						src={img}
-						alt={`${friendly} Wikipedia article image`}
+						controls
 					/>
 				{/if}
 				{#if typeof blurb === 'string'}
