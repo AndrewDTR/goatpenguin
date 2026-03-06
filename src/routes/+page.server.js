a45bb7d7fee21da13942bf682d5b49814d87e84e
Andrew <74398819+AndrewDTR@users.noreply.github.com>
2026-03-05 23:56:15 -0600

    day 52

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -5,21 +5,21 @@ export async function load({ url }) {
         redirect(308, 'https://goatpenguin.com');
     }
 
-    const article = "Women_and_children_first";
-    const friendly = "Women and children first";
-    const acceptedGuesses = ["women and children first", "women and the children first", "the women and the children first", "children and women first"];
-    const day = "day51";
-    const dayNum = 51;
+    const article = "WinRAR";
+    const friendly = "WinRAR";
+    const acceptedGuesses = ["winrar", "win rar"];
+    const day = "day52";
+    const dayNum = 52;
 
-    const categories = ["Chivalry",
-        "1840s quotations",
-        "Women's rights",
-        "Sinking of RMS Titanic",
-        "Lifeboats",
+    const categories = ["1995 software",
+        "Disk image extractors",
+        "Windows-only trialware",
+        "File archivers",
+        "Windows compression software",
     ]
 
-    const blurb = [`"Women and children first", known to a lesser extent as the Birkenhead drill, is an unofficial code of conduct and gender role whereby the lives of women and children were to be saved first in a life-threatening situation, typically abandoning ship, when survival resources such as lifeboats were limited.`, `In the 19th and early 20th century, "women and children first" was seen as a chivalric ideal. The concept "was celebrated among Victorian and Edwardian commentators as a long-standing practice - a 'tradition', 'law of human nature', 'the ancient chivalry of the sea', 'handed down in the race'." Its practice was featured in accounts of some 18th-century shipwrecks with greater public awareness during the 19th century.`];
-    const img = "https://upload.wikimedia.org/wikipedia/commons/3/37/Wreck_of_the_Birkenhead.jpg";
+    const blurb = `WinRAR is a trialware file archiver and compression utility for Microsoft Windows, developed by Eugene Roshal of win.rar GmbH (RARLAB) since 1995. It can create and view archives in RAR (a format created by Roshal) or ZIP file formats, and unpack numerous archive file formats. To enable the user to test the integrity of archives, WinRAR embeds CRC-32 or BLAKE2 checksums for each file in each archive. WinRAR supports creating encrypted, multi-part and self-extracting archives.`;
+    const img = "https://upload.wikimedia.org/wikipedia/en/4/43/WinRAR_7.10_screenshot.png";
 
     return {
         article,
