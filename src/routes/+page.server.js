1b383b65ef01ce2a2a0348b3c5e1027d022f2477
Andrew <74398819+AndrewDTR@users.noreply.github.com>
2026-03-12 11:32:10 -0500

    day 58

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -5,21 +5,21 @@ export async function load({ url }) {
         redirect(308, 'https://goatpenguin.com');
     }
 
-    const article = "Google_Glass";
-    const friendly = "Google Glass";
-    const acceptedGuesses = ["google glass"];
-    const day = "day57";
-    const dayNum = 57;
+    const article = "Flappy_Bird";
+    const friendly = "Flappy Bird";
+    const acceptedGuesses = ["flappy bird"];
+    const day = "day58";
+    const dayNum = 58;
 
-    const categories = ["Products introduced in 2013",
-        "Android (operating system) devices",
-        "Google hardware",
-        "Wearable computers",
-        "Eyewear brands of the United States",
+    const categories = ["2010s fads and trends",
+        "Plagiarism controversies in video games",
+        "Side-scrolling video games ",
+        "IOS games",
+        "Video games about birds",
     ]
 
-    const blurb = [`Google Glass, or simply Glass, is a discontinued brand of smart glasses developed by Google's X Development (formerly Google X), with a mission of producing a ubiquitous computer. Google Glass displays information to the wearer using a head-up display. Wearers communicate with the Internet via natural language voice commands.`, `Google started selling a prototype of Google Glass to qualified "Glass Explorers" in the US on June 27, 2012, for a limited period for $1,500, (with distribution of those purchases beginning on April 16, 2013), before it became available to the public on April 15, 2014. It has an integrated 5 megapixel still/720p video camera. The headset received a great deal of criticism amid concerns that its use could violate existing privacy laws.`];
-    const img = "https://upload.wikimedia.org/wikipedia/commons/7/7b/Google_Glass_photo.JPG";
+    const blurb = `Flappy Bird is a 2013 casual mobile game developed by Vietnamese video game artist and programmer Dong Nguyen (Vietnamese: Nguyễn Hà Đông), under his game development company .Gears. The game is a side-scroller where the player controls a bird, Faby, attempting to fly between columns of green pipes without hitting them. The player's score is determined by the number of pipes they pass. Nguyen created the game over a period of several days, using the bird from a cancelled game made in 2012.`;
+    const img = "https://upload.wikimedia.org/wikipedia/en/0/0a/Flappy_Bird_icon.png";
     
     return {
         article,
