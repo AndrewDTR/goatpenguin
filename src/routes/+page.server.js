1c33e3dd11bb9d316b2b2ae3be6da1bbda229774
Andrew <74398819+AndrewDTR@users.noreply.github.com>
2026-03-13 02:57:57 -0500

    day 59

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -5,21 +5,21 @@ export async function load({ url }) {
         redirect(308, 'https://goatpenguin.com');
     }
 
-    const article = "Flappy_Bird";
-    const friendly = "Flappy Bird";
+    const article = "Let_them_eat_cake";
+    const friendly = "Let them eat cake";
     const acceptedGuesses = ["flappy bird"];
-    const day = "day58";
-    const dayNum = 58;
+    const day = "day59";
+    const dayNum = 59;
 
-    const categories = ["2010s fads and trends",
-        "Plagiarism controversies in video games",
-        "Side-scrolling video games ",
-        "IOS games",
-        "Video games about birds",
+    const categories = ["1760s neologisms",
+        "English phrases",
+        "Political quotes",
+        "Metaphors referring to food and drink",
+        "Works by Jean-Jacques Rousseau",
     ]
 
-    const blurb = `Flappy Bird is a 2013 casual mobile game developed by Vietnamese video game artist and programmer Dong Nguyen (Vietnamese: Nguyễn Hà Đông), under his game development company .Gears. The game is a side-scroller where the player controls a bird, Faby, attempting to fly between columns of green pipes without hitting them. The player's score is determined by the number of pipes they pass. Nguyen created the game over a period of several days, using the bird from a cancelled game made in 2012.`;
-    const img = "https://upload.wikimedia.org/wikipedia/en/0/0a/Flappy_Bird_icon.png";
+    const blurb = [`"Let them eat cake" is the traditional translation of the French phrase "Qu'ils mangent de la brioche", said to have been spoken in the 18th century by "a great princess" upon being told that the peasants had no bread. The French phrase mentions brioche, a bread enriched with butter and eggs, considered a luxury food. The quote is taken to reflect either the princess's frivolous disregard for the starving peasants or her poor understanding of their plight.`, `Although the phrase is conventionally attributed to Marie Antoinette, there is no evidence that she ever uttered it, and it is now generally regarded as a journalistic cliché. The phrase can actually be traced back to Jean-Jacques Rousseau's Confessions in 1765, 24 years prior to the French Revolution, and when Antoinette was nine years old and had never been to France. The phrase was not attributed to Antoinette until decades after her death.`];
+    const img = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Allan_Ramsay_-_Jean-Jacques_Rousseau_%281712_-_1778%29_-_Google_Art_Project.jpg/1280px-Allan_Ramsay_-_Jean-Jacques_Rousseau_%281712_-_1778%29_-_Google_Art_Project.jpg";
     
     return {
         article,
