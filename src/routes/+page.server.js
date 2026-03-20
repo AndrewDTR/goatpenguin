c87a60df67e8b2942a474404b9447a82c5dc198d
Andrew <74398819+AndrewDTR@users.noreply.github.com>
2026-03-20 00:16:15 -0500

    day 66

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -5,21 +5,21 @@ export async function load({ url }) {
         redirect(308, 'https://goatpenguin.com');
     }
 
-    const article = "Lorem_ipsum";
-    const friendly = "Lorem ipsum";
-    const acceptedGuesses = ["lorem ipsum", "lorem ispum", "lorum ipsum", "lorum ispum"];
-    const day = "day65";
-    const dayNum = 65;
+    const article = "Boomerang";
+    const friendly = "Boomerang";
+    const acceptedGuesses = ["boomerang"];
+    const day = "day66";
+    const dayNum = 66;
 
-    const categories = ["1960s neologisms",
-        "Cicero",
-        "Latin words and phrases",
-        "Nonsense",
-        "Filler text",
+    const categories = ["1790s neologisms",
+        "Sports equipment",
+        "Physical activity and dexterity toys",
+        "Hunting equipment",
+        "Throwing clubs",
     ]
 
-    const blurb = `Lorem ipsum is a dummy or placeholder text commonly used in graphic design, publishing, and web development. It is typically a corrupted version of De finibus bonorum et malorum, a 1st-century BC text by the Roman statesman and philosopher Cicero, with words altered, added, and removed to make it nonsensical and improper Latin. The first two words are the truncation of dolorem ipsum ("pain itself"). Lorem ipsum's purpose is to permit a page layout to be designed, independently of the copy that will subsequently populate it, or to demonstrate various fonts of a typeface without meaningful text that could be distracting.`;
-    const img = "https://upload.wikimedia.org/wikipedia/commons/4/42/Letraset_Lorem_Ipsum.jpg";
+    const blurb = `A boomerang is a thrown tool typically constructed with airfoil sections and designed to spin about an axis perpendicular to the direction of its flight, designed to return to the thrower. The origin of the word is from an Aboriginal Australian language of the Sydney region. Its original meaning, which is preserved in official competitions, refers only to returning objects, not to throwing sticks, which were also used for hunting by various peoples both in Australia and around the world. However, the term "non-returning boomerang" is also in general use. Various forms of boomerang-like designs were traditionally and in some cases are still used by some groups of Aboriginal Australians for hunting. The tools were known by various names in the many Aboriginal languages prior to colonisation. The oldest surviving Aboriginal boomerang, now held in the South Australian Museum, was found in a peat bog in South Australia, dated to 10,000 BC. Historically, boomerangs have been used for hunting, sport, and entertainment, and are made in various shapes and sizes to suit different purposes. Ancient "boomerangs", used for hunting, have also been discovered in Egypt, the Americas, and Europe, although it is unclear whether any of these were of the returning type.`
+    const img = "https://upload.wikimedia.org/wikipedia/commons/9/91/Boomerangs_-_melbourne_show_2005.jpg";
 
     return {
         article,
