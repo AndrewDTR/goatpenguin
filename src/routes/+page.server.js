65ccb3440e6c7b38ed71ffd68dc35a73423631d1
Andrew <74398819+AndrewDTR@users.noreply.github.com>
2026-03-21 17:17:09 -0500

    day 67

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -5,21 +5,21 @@ export async function load({ url }) {
         redirect(308, 'https://goatpenguin.com');
     }
 
-    const article = "Boomerang";
-    const friendly = "Boomerang";
-    const acceptedGuesses = ["boomerang"];
-    const day = "day66";
-    const dayNum = 66;
+    const article = "Dyson_(company)";
+    const friendly = "Dyson (company)";
+    const acceptedGuesses = ["dyson"];
+    const day = "day67";
+    const dayNum = 67;
 
-    const categories = ["1790s neologisms",
-        "Sports equipment",
-        "Physical activity and dexterity toys",
-        "Hunting equipment",
-        "Throwing clubs",
+    const categories = ["Companies of Singapore",
+        "Electronics companies established in 1993",
+        "Companies named after their founders",
+        "Engineering companies of the United Kingdom",
+        "Vacuum cleaner manufacturers",
     ]
 
-    const blurb = `A boomerang is a thrown tool typically constructed with airfoil sections and designed to spin about an axis perpendicular to the direction of its flight, designed to return to the thrower. The origin of the word is from an Aboriginal Australian language of the Sydney region. Its original meaning, which is preserved in official competitions, refers only to returning objects, not to throwing sticks, which were also used for hunting by various peoples both in Australia and around the world. However, the term "non-returning boomerang" is also in general use. Various forms of boomerang-like designs were traditionally and in some cases are still used by some groups of Aboriginal Australians for hunting. The tools were known by various names in the many Aboriginal languages prior to colonisation. The oldest surviving Aboriginal boomerang, now held in the South Australian Museum, was found in a peat bog in South Australia, dated to 10,000 BC. Historically, boomerangs have been used for hunting, sport, and entertainment, and are made in various shapes and sizes to suit different purposes. Ancient "boomerangs", used for hunting, have also been discovered in Egypt, the Americas, and Europe, although it is unclear whether any of these were of the returning type.`
-    const img = "https://upload.wikimedia.org/wikipedia/commons/9/91/Boomerangs_-_melbourne_show_2005.jpg";
+    const blurb = `Dyson Limited, d.b.a Dyson, is a Singaporean–British multinational technology company. Founded in 1991 by James Dyson in Malmesbury, Wiltshire, England, the company designs and manufactures household appliances such as vacuum cleaners, air purifiers, hand dryers, bladeless fans, heaters, hair dryers, and lights. As of 2022 Dyson has more than 14,000 employees worldwide. In 2019, Dyson moved its headquarters from the United Kingdom to Singapore to be closer to its manufacturing and supply-chain hubs and Asian customer base.`;
+    const img = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Dyson_Demo_Store%2C_London.jpg/1280px-Dyson_Demo_Store%2C_London.jpg";
 
     return {
         article,
