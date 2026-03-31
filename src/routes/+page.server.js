bf45a3df0e0f0b856f0dec4a07eb2f8a98cefef9
Andrew <74398819+AndrewDTR@users.noreply.github.com>
2026-03-31 03:01:23 -0400

    day 71

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -5,21 +5,21 @@ export async function load({ url }) {
         redirect(308, 'https://goatpenguin.com');
     }
 
-    const article = "Sundial";
-    const friendly = "Sundial";
-    const acceptedGuesses = ["sundial", "sun dial"];
-    const day = "day70";
-    const dayNum = 70;
+    const article = "Great_Filter";
+    const friendly = "Great Filter";
+    const acceptedGuesses = ["great filter", "the great filter"];
+    const day = "day71";
+    const dayNum = 71;
 
-    const categories = ["Outdoor sculptures",
-        "Egyptian inventions",
-        "Garden ornaments",
-        "Horology",
-        "Clocks",
+    const categories = ["1996 in science",
+        "Open problems",
+        "Extraterrestrial life",
+        "Doomsday scenarios",
+        "Fermi paradox",
     ]
 
-    const blurb = `A sundial is a horological device that tells the time of day (referred to as civil time in modern usage) when direct sunlight shines by the apparent position of the Sun in the sky. In the narrowest sense of the word, it consists of a flat plate (the dial) and a gnomon, which casts a shadow onto the dial. As the Sun appears to move through the sky, the shadow aligns with different hour-lines, which are marked on the dial to indicate the time of day. The style is the time-telling edge of the gnomon, though a single point or nodus may be used. The gnomon casts a broad shadow; the shadow of the style shows the time. The gnomon may be a rod, wire, or elaborately decorated metal casting. The style must be parallel to the axis of the Earth's rotation for the sundial to be accurate throughout the year. The style's angle from horizontal is equal to the sundial's geographical latitude.`;
-    const img = "https://upload.wikimedia.org/wikipedia/commons/8/86/MootHallSundial.JPG";
+    const blurb = `The Great Filter is the idea that, in the development of life from the earliest stages of abiogenesis to reaching the highest levels of development on the Kardashev scale, there is a barrier to development that makes detectable extraterrestrial life exceedingly rare. The Great Filter is one possible resolution of the Fermi paradox. The main conclusion of the Great Filter is that there is an inverse correlation between the probability that other life could evolve to the present stage in which humanity is, and the chances of humanity to survive in the future.`;
+    const img = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Meteosat-12-fci-march-equinox-2025-noon.jpg/1280px-Meteosat-12-fci-march-equinox-2025-noon.jpg";
 
     return {
         article,
