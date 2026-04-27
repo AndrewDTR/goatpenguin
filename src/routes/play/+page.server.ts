129075df35e39f315767137148549eb526f54ba5
Andrew <74398819+AndrewDTR@users.noreply.github.com>
2026-04-27 15:42:23 -0500

    prevent future goatpenguin peeping

--- src/routes/play/+page.server.ts
+++ src/routes/play/+page.server.ts
@@ -1,14 +1,47 @@
+import { isAuthed } from '$lib/server/auth.js';
 import { error } from '@sveltejs/kit';
 
-export async function load({ fetch, url }) {
-	const dayString = url.searchParams.get("day")
+export async function load({ fetch, url, cookies }) {
+	const dayAfter = (curr: string, test: string) => {
+		// true if we're after the curr date
+		const currSplit = curr.split('-').map((s) => Number(s));
+		const testSplit = test.split('-').map((s) => Number(s));
+
+		if (currSplit[0] > testSplit[0]) {
+			return false;
+		}
+
+		if (currSplit[0] == testSplit[0]) {
+			if (currSplit[1] > testSplit[1]) {
+				return false;
+			}
+
+			if (currSplit[1] == testSplit[1]) {
+				return currSplit[2] < testSplit[2];
+			}
+		}
+
+		return true;
+	};
+
+	const dayString = url.searchParams.get('day') ?? '';
 
 	const gameRes = await fetch(`/game?day=${dayString}`);
 	const game = await gameRes.json();
 
 	if (gameRes.status == 404) {
 		error(404, {
-			message: "Can't find today's goatpenguin..."
+			message: "Can't find that goatpenguin..."
+		});
+	}
+
+	const curr = new Intl.DateTimeFormat('sv-SE', {
+		timeZone: 'America/Chicago'
+	}).format(new Date());
+
+	if (!isAuthed(cookies.get('admin')) && dayAfter(curr, dayString)) {
+		error(403, {
+			message: "Can't access future goatpenguins..."
 		});
 	}
 
